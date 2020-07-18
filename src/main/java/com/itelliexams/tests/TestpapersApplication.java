256c867b46fc9f14a9bb8b0156b861d151fa2ac0
Bhavya Arora <arorabhavya23@gmail.com>
2020-07-18 16:42:40 +0530

    gradle project setup

---
+++ src/main/java/com/itelliexams/tests/TestpapersApplication.java
@@ -0,0 +1,13 @@
+package com.itelliexams.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpapersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpapersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itelliexams/tests/TestpapersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itelliexams.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpapersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
